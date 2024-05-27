2b8008ed6f43588af4594d7a28a6bf08fe743417
XDuskAshes <thegaymeradummy@gmail.com>
2024-05-26 23:52:37 -0400

    Revamp entirely
    Rewrote stitch top-to-bottom, introducing use of JSON and better build procedures.

--- docs/readme.txt
+++
@@ -1,4 +0,0 @@
-Use a LaTeX compiler of some kind to compile this into a readable doc.
-I used pdflatex on Arch. (I use arch btw)
-
-Either that, or read the PDF provided at 'completed/' when I finish documentation.
--- stitch.lua
+++ stitch.lua
@@ -1,85 +1,166 @@
 -- Stitches files together into a single output.
 -- Copyright (C) 2024 XDuskAshes
--- Licensed under the MIT License -  <https://opensource.org/license/mit>
+-- Licensed under the MIT License - <https://opensource.org/license/mit>
 
 local args = {...}
+local ver = 1.0
+local jsonfile = args[1]
 
-if args[1] == "--help" or args[1] == "-h" or args[1] == "-?" then
-	print("stitch | Cobbles files together efficiently.*")
-	print("--------------------------------------------")
-	print("--help/-h/-? | Show this help.")
-end
-
-local function e(s)
-    return s == "" or s == nil
-end
-
-if e(args[1]) then
-    error("Need a file. See docs.",0)
-end
-
-if e(args[2]) then
-    error("Need a file to output to. See docs.",0)
-end
-
-local function isAFile(s)
-    if not fs.exists(s) then
-        return false
+if not fs.exists(args[1]) then
+    if not fs.exists("/"..shell.dir().."/"..args[1]) then
+        error("Cannot find: "..args[1],0)
     else
-        if fs.isDir(s) then
-            return false
-        else
-            return true
+        jsonfile = "/"..shell.dir().."/"..args[1]
+    end
+end
+
+local handle = fs.open(jsonfile,"r")
+local build = textutils.unserialiseJSON(handle.readAll())
+
+local replaceTarget = build.opts.replaceTarget
+local verbose = build.opts.verbose
+local ignoreMissing = build.opts.ignoreMissing
+local produceLog = build.opts.produceLog
+
+if #build.files < 2 then
+    print("Do you just want to move a single file? You know you can use 'move' for that right?")
+    print("try 'move "..build.srcdir..build.files[1].." "..build.target.."'")
+    error()
+end
+
+local listFiles = ""
+print("Build name: "..build.name)
+print("Target file: "..build.target)
+print("Source files directory: "..build.srcdir)
+write("Files to build with: ")
+for k,v in pairs(build.files) do
+    term.setTextColor(colors.green)
+    write(v.." ")
+    term.setTextColor(colors.white)
+    listFiles = listFiles..v.." "
+end
+print("\n")
+
+print("Ensuring needed files in "..build.srcdir.." exist for build...")
+local missingFiles = {}
+for k,v in pairs(build.files) do
+    if fs.exists(build.srcdir..v) then
+        print(v..": yes")
+    else
+        print(v.." :no")
+        table.insert(missingFiles,v)
+    end
+    term.setTextColor(colors.white)
+end
+
+if #missingFiles > 0 then
+    if #missingFiles == 1 then
+        printError("A file is not present in "..build.srcdir..": "..missingFiles[1])
+    else
+        printError("Multiple files are not present in "..build.srcdir..":")
+        for k,v in pairs(missingFiles) do
+            print(v)
         end
     end
-end
 
-local arg1IsFile = isAFile(args[1])
-local arg2Exists = isAFile(args[2])
-
-if arg1IsFile == false then
-    error("Either '"..args[1].."' doesn't exist or is a directory.",0)
-end
-
-if arg2Exists == true then
-    error("File '"..args[2].."' already exists.",0)
-elseif fs.isDir(args[2]) then
-    error("'"..args[2].."' is a directory.",0)
-end
-
-local handle
-
-local instructions = {}
-
-handle = fs.open(args[2],"w")
-handle.writeLine("")
-handle.close()
-
-handle = fs.open(args[1],"r")
-
-repeat
-    local a = handle.readLine()
-    table.insert(instructions,a)
-until e(a)
-
-handle.close()
-
-for k,v in pairs(instructions) do
-    print(k,v)
-    local toWrite = {}
-
-    handle = fs.open(v,"r")
-    repeat
-        local a = handle.readLine()
-        table.insert(toWrite,a)
-    until a == "--<eof>"
-    handle.close()
-
-    table.remove(toWrite,#toWrite)
-
-    handle = fs.open(args[2],"a")
-    for k,v in pairs(toWrite) do
-        handle.writeLine(v)
+    if not ignoreMissing then
+        print("And we are not ignoring failures, so error out.")
+        print("=====\nMake sure you have all files needed listed in "..args[1]..", or change 'ignoreMissing' to 'false' in "..args[1]..".")
+        error()
+    else
+        print("And we are ignoring failures, so continuing.")
     end
-    handle.close()
+else
+    print("All files present")
+end
+
+local function readFile(path)
+    if not fs.exists(path) then
+        return nil, "File does not exist: " .. path
+    end
+    local file = fs.open(path, "r")
+    local content = file.readAll()
+    file.close()
+    return content
+end
+
+if verbose then
+    print("-- STARTING BUILD --")
+
+    if fs.exists(build.target) then
+        write("Target already exists: "..build.target..",")
+        if not replaceTarget then
+            print("and we don't want to replace it, so error.")
+            print("=====\nMake sure to either delete "..build.target.." or change the 'replaceTarget' value to 'true' in "..args[1]..".")
+            error()
+        else
+            print("and we can replace it, so do that.")
+            fs.delete(build.target)
+            handle = fs.open(build.target,"w")
+            handle.close()
+        end
+    else
+        print("Target "..build.target.." doesn't exist, fresh build")
+        handle = fs.open(build.target,"w")
+        handle.close()
+    end
+
+    for k,v in pairs(build.files) do
+        print(build.srcdir..v.." > "..build.target)
+
+        local contents, err = readFile(build.srcdir..v)
+        if contents then
+            handle = fs.open(build.target,"a")
+            handle.writeLine(contents)
+        else
+            error("Issue reading file "..build.srcdir..v..": "..err)
+        end
+        
+        handle.close()
+    end
+    print("-- FINISHED BUILD --")
+    write("Verifying "..build.target.." exists: ")
+    if fs.exists(build.target) then
+        print("yes")
+    else
+        error("no\nFile does not exist: "..build.target..", what went wrong?")
+    end
+else
+    write("Building: ")
+
+    if fs.exists(build.target) then
+        if not replaceTarget then
+            print("Not allowed to replace: "..build.target)
+            print("=====\nMake sure to either delete "..build.target.." or change the 'replaceTarget' value to 'true' in "..args[1]..".")
+            error()
+        else
+            fs.delete(build.target)
+            handle = fs.open(build.target,"w")
+            handle.close()
+        end
+    else
+        handle = fs.open(build.target,"w")
+        handle.close()
+    end
+
+    for k,v in pairs(build.files) do
+        local contents, err = readFile(build.srcdir..v)
+        if contents then
+            handle = fs.open(build.target,"a")
+            handle.writeLine(contents)
+        else
+            error("Issue reading file "..build.srcdir..v..": "..err)
+        end
+        
+        handle.close()
+    end
+
+    print("done")
+
+    write("Verifying build target exists: ")
+    if fs.exists(build.target) then
+        print("yes")
+    else
+        error("no\nFile does not exist: "..build.target..", what went wrong?")
+    end
 end
